098e4d033a2adc1227e43a92ba8c2703d54f916d
William Gough <williamhenrygough@gmail.com>
2017-01-29 14:51:09 +0000

    basic http server with form submission & gzip

--- 004_http_servers/main.go
+++ 004_http_servers/main.go
@@ -1,5 +1,38 @@
 package main
 
-func main() {
+import (
+	"net/http"
+	"github.com/nytimes/gziphandler"
+	"log"
+	"net/url"
+	"html/template"
+)
 
+type handle int
+
+func (m handle) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+	err := req.ParseForm()
+	if err != nil {
+		log.Println(err)
+	}
+
+	data := struct {
+		Submissions url.Values
+	}{
+		req.Form,
+	}
+
+	tpl.ExecuteTemplate(res, "index.gohtml", data)
+}
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseFiles("templates/index.gohtml"))
+}
+
+func main() {
+	var d handle
+	withGz := gziphandler.GzipHandler(d)
+	http.ListenAndServe(":8080", withGz)
 }
