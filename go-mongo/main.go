35700d30a4e0f06dbcf89409549212fba3421b83
William Gough <williamhenrygough@gmail.com>
2017-01-31 21:35:17 +0000

    go-mongo: add getUser route

--- go-mongo/main.go
+++ go-mongo/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/julienschmidt/httprouter"
 	"github.com/nytimes/gziphandler"
+	"github.com/williamhgough/go-web-dev/go-mongo/models"
 	"html/template"
 	"net/http"
 )
@@ -12,6 +15,7 @@ var tpl *template.Template
 func main() {
 	server := httprouter.New()
 	server.GET("/", index)
+	server.GET("/user/:id", getUser)
 	zipped := gziphandler.GzipHandler(server)
 	http.ListenAndServe("localhost:8080", zipped)
 }
@@ -21,5 +25,23 @@ func init() {
 }
 
 func index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	w.Header().Set("Content-Type", "text/html; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
 	tpl.ExecuteTemplate(w, "index.gohtml", nil)
+}
+
+func getUser(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	u := models.User{
+		Name:   "William Gough",
+		Gender: "male",
+		Age:    23,
+		Id:     p.ByName("id"),
+	}
+
+	// Marshal into Json
+	uj, _ := json.Marshal(u)
+	// Write content type, status code
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "%s\n", uj)
 }
